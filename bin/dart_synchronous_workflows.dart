bcebc3fe21863c0b04dfa9d78719ff38424b78e2
Eugene <eugeneayonga@gmail.com>
2023-05-12 07:49:46 +0300

    First Commit

---
+++ bin/dart_synchronous_workflows.dart
@@ -0,0 +1,5 @@
+import 'package:dart_synchronous_workflows/dart_synchronous_workflows.dart' as dart_synchronous_workflows;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_synchronous_workflows.calculate()}!');
+}
---
+++ test/dart_synchronous_workflows_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_synchronous_workflows/dart_synchronous_workflows.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
